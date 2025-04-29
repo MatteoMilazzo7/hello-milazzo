fb87af180d0a6215332a56da57ab7123bdaee4c0
MatteoMilazzo7 <matteo.milazzo@capellinisauro.edu.it>
2025-04-29 14:45:31 +0000

    Stampa la squadra del cuore

--- HelloMilazzo.java
+++ HelloMilazzo.java
@@ -5,5 +5,7 @@ public class HelloMilazzo {
         System.out.println("Hello Milazzo"); //Stampo 3 volte Hello Milazzo
 
         System.out.println("2/07/2007"); //Stampo la data di nascita
+
+        System.out.println("Forza Catania !"); //Stampo la mia squadra del cuore
     }
 }
