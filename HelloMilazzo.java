04881807b7bc0c375d9fb833b8b5cc9cb3ce44d8
MatteoMilazzo7 <matteo.milazzo@capellinisauro.edu.it>
2025-04-29 14:39:44 +0000

    “Commit

---
+++ HelloMilazzo.java
@@ -0,0 +1,7 @@
+public class HelloMilazzo {
+	public static void main(String[] args) {
+		System.out.println("Hello Milazzo");
+        System.out.println("Hello Milazzo");
+        System.out.println("Hello Milazzo"); //Stampo 3 volte Hello Milazzo
+    }
+}
