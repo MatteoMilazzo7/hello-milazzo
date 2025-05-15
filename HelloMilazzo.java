dbd14546c3cb1eb436339187cc8ba8eb9b9da6e4
Lelone77 <emanuele.paita@capellinisauro.edu.it>
2025-05-15 06:25:26 +0000

    Modifica del codice dal compagno

--- HelloMilazzo.java
+++ HelloMilazzo.java
@@ -7,5 +7,7 @@ public class HelloMilazzo {
         System.out.println("2/07/2007"); //Stampo la data di nascita
 
         System.out.println("Forza Catania !"); //Stampo la mia squadra del cuore
+
+        System.out.println("Ciao, ho modificato il codice! --Emanuele");
     }
 }
